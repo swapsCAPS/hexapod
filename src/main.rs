8feba8eb1947cd8691f26cd7ed433e00a95718e0
swapsCAPS <verstegen.daan@gmail.com>
2019-04-06 16:40:24 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,91 @@
-struct Servo {
-    pin: u8,
-    min: u64,
-    max: u64
+use std::{thread, time};
+
+struct Joint {
+  pin: u8,
+  min: u64,
+  max: u64
 }
+
+enum LegType { Front, Middle, Back }
 
 // Each leg for one side will have different motions asociated with a step... I.e. back legs will
 // behave differently from front legs
 struct Leg {
-    pelvis: Servo,
-    knee: Servo,
-    ankle: Servo
+  leg_type: LegType,
+  pelvis: Joint,
+  knee: Joint,
+  ankle: Joint
 }
+
 impl Leg {
-    fn new(&self, pelvis: Servo, knee: Servo, ankle: Servo) -> Leg {
-        Leg { pelvis, knee, ankle }
+  fn new(leg_type: LegType, pelvis: Joint, knee: Joint, ankle: Joint) -> Leg {
+    Leg { leg_type, pelvis, knee, ankle }
+  }
+
+  fn step(&self, dir: u16) -> () {
+    // match leg_type
+    println!("Step!");
+    match self.leg_type {
+      LegType::Front => {
+        println!("Moving front leg");
+      }
+      _ => {
+        println!("Not implemented!");
+      }
     }
-    fn step(&self, dir: u16) -> () {
-        // Take one step in a direction. I guess we need to define NESW dirs and then do some
-        // interpolation magic to get to the other 355 degrees...
-    }
-    fn rest(&self) -> () {
-        // Go into resting position for this leg
-    }
+
+  }
+
+  fn rest(&self) -> () {
+    // Go into resting position for this leg
+  }
+
 }
 
 struct Brain {
-    fl: Leg,
-    fr: Leg,
-    ml: Leg,
-    mr: Leg,
-    bl: Leg,
-    br: Leg
+  fl: Leg,
+  // fr: Leg,
+  // ml: Leg,
+  // mr: Leg,
+  // bl: Leg,
+  // br: Leg
 }
 impl Brain {
-    fn walk(&self, speed: u8, dir: u16) -> () {
+  fn new() -> Brain {
+    println!("New brain!");
 
+    let fl = Leg::new(
+      LegType::Front,
+      Joint { pin: 0, min: 0, max: 180 },
+      Joint { pin: 1, min: 0, max: 180 },
+      Joint { pin: 2, min: 0, max: 180 },
+    );
+
+    Brain {
+      fl: fl
     }
+  }
+
+  fn walk(&self, speed: u64, dir: u16) -> () {
+    self.fl.step(dir);
+    thread::sleep(time::Duration::from_millis(speed));
+    self.fl.step(dir);
+
+  }
 }
 
 fn main() {
 
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_walk() {
+    let brain = Brain::new();
+    brain.walk(1000, 0);
+
+  }
 }
